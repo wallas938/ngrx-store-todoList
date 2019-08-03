abc2baf99a8aac7bd57d2a40acdc4a4ec38b105f
wallas938 <33827350+wallas938@users.noreply.github.com>
2019-08-03 09:36:09 +0200

    debugage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+/* Materials NgModule start*/
+import {MatToolbarModule} from '@angular/material/toolbar';
+/* Materials NgModule end*/
 
 import { AppComponent } from './app.component';
+import { TodolistComponent } from './containers/todolist/todolist.component';
+import { TodoComponent } from './containers/todolist/todo/todo.component';
+import { NewTodoComponent } from './containers/new-todo/new-todo.component';
+import { FooterComponent } from './containers/todolist/footer/footer.component';
+
+const appRoutes: Routes = [
+  { path: 'todo-list', component: TodolistComponent },
+  { path: 'new-todo', component: TodoComponent },
+  { path: '', redirectTo: 'todo-list', pathMatch: 'full'},
+  { path: '**', redirectTo: 'todo-list'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodolistComponent,
+    TodoComponent,
+    NewTodoComponent,
+    FooterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/containers/new-todo/new-todo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-todo',
+  templateUrl: './new-todo.component.html',
+  styleUrls: ['./new-todo.component.css']
+})
+export class NewTodoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
